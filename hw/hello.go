2b5560abd8cfd34f57b0c1297b79e7bffff17719
Tony-S121 <t.smith9300@outlook.com>
2025-07-21 00:54:00 +0100

    Add subtests

--- hw/hello.go
+++ hw/hello.go
@@ -4,9 +4,12 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 func Hello(name string) string {
+	if name == ""{
+		name= "World!"
+	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Tony"))
+	fmt.Println(Hello(""))
 }
