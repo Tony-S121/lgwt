99afe03a77485a76d3d7474f9c7fce5212fd36f6
Tony-S121 <t.smith9300@outlook.com>
2025-07-21 22:16:42 +0100

    Add switch and new private func

--- hw/hello_test.go
+++ hw/hello_test.go
@@ -4,17 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T){
-		got := Hello("Tony")
+		got := Hello("Tony", "")
 		want := "Hello, Tony"
 		assertCorrectMessage(t, got, want)
 
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T){
-		got := Hello("")
-		want := "Hello, World!"
+		got := Hello("", "")
+		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
-}
+	t.Run("In Spanish", func(t * testing.T){
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("In French", func(t * testing.T){
+		got := Hello("", "French")
+		want := "Bonjour, World"
+		assertCorrectMessage(t, got, want)
+	})
+ 
+ }
+
 
 func assertCorrectMessage(t testing.TB, got, want string){
 	t.Helper()
