7be7cdb3ba53e90cab8b475a2fabcc60a5b1dcc0
Tony-S121 <t.smith9300@outlook.com>
2025-07-24 23:44:24 +0100

    Add first section of arrays

---
+++ arrays/sum.go
@@ -0,0 +1,13 @@
+package arrays
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	return nil
+}
