c7c7a5391e0d726ac40906e6f4461b0c99744959
Tony-S121 <t.smith9300@outlook.com>
2025-07-29 23:04:53 +0100

    Add error handling and tests

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,13 +1,38 @@
 package pointers
 
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficentFunds = errors.New("cannot withdraw, insufficient funds")
+
 type Wallet struct {
-	balance float64
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount float64) {
+type Bitcoin int
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() float64 {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficentFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
